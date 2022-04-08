4beb863a5f65e4f1f988ed0dd2ab40a4f61f5229
José Pedro <josepedro23zx@gmail.com>
2022-04-08 10:49:45 -0300

    Config method host

--- database/db.js
+++ database/db.js
@@ -1,18 +1,31 @@
 require('dotenv/config')
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
+var sequelize
+var notice
+
+if(process.env.DATABASE_URL){
+  notice = "connected to the HEROKU server"
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
       }
-    }
+  })
+}
+else{
+  notice = "connected to the LOCAL server"
+  sequelize = new Sequelize(process.env.DATABASE_LOCAL, {
+    dialect: 'postgres'
 })
+}
+
 
 sequelize.authenticate().then(() => {
-    console.log("Success!");
+    console.log(notice);
   }).catch((err) => {
     console.log(err);
   });
