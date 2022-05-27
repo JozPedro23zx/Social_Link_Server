9aedee772c4aa246ded505abade52b8f0e6b089d
José Pedro <josepedro23zx@gmail.com>
2022-05-27 14:24:42 -0300

    hcasbjh

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(session({
+    sotore: new (require('connect-pg-simple')(session))(),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
