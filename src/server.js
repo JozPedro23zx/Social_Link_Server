7177c9225d1086e9feb87e7b8359cf70e3802404
JozPedro23zx <josepedro23zx@gmail.com>
2023-05-27 09:02:50 -0300

    Update socket io

--- src/server.js
+++ src/server.js
@@ -7,8 +7,7 @@ const session = require('express-session');
 const cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const MessageBoxService = require('./Services/MessageBoxService');
+const socketHandler = require('./socket-events')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -37,63 +36,7 @@ app.use((req, res, next) =>{
 });
 
 
-const io = new Server(server, {
-    cors: {
-        origin: `${process.env.FRONTEND}`,
-        methods: ["GET", "POST"],
-    },
-    transports: ['websocket']
-});
-
-
-io.on("connection", async (socket) =>{
-    console.log("User connect:", socket.id)
-
-    // socket.data.username = "123h"
-    // const sockets = await io.fetchSockets()
-    
-    // user = sockets.find((userSocket)=>{return userSocket.data.username === '123h'})
-    // console.log("User connect:", user.id)
-
-    socket.on("set_user_id", async (data) =>{
-        socket.data.userId = data[0]
-        console.log('User id:',socket.data.userId)
-    })
-
-    socket.on("send_notification", async ({idRecipient, idSender, type})=>{
-        const sockets = await io.fetchSockets()
-        const userSocket = sockets.find((user)=>{return user.data.userId === idRecipient})
-        
-        if(userSocket){
-            io.to(userSocket.id).emit("get_notification", {
-                idSender,
-                type
-            })
-        }
-        MessageBoxService.SendMessage(type, idRecipient, idSender)
-
-    })
-
-    socket.on("join_room", (data) =>{
-        socket.join(data)
-        console.log(`User with ID: ${socket.id} JOINED room: ${data}`)
-    })
-
-    socket.on("leave_room", (data) =>{
-        socket.leave(data)
-        console.log(`User with ID: ${socket.id} LEAVED room: ${data}`)
-    })
-
-    socket.on("send_message", (data) =>{
-        socket.to(data.roomId).emit("receive_message", data)
-    })
-
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnect: ", socket.id)
-        console.log("=================================")
-    })
-});
-
+socketHandler(server)
 
 app.use(router);
 
